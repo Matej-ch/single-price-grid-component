0dd58716d5a14b794e5ca7ed2994393e0bc72d41
Matej <matejchalachan@gmail.com>
2022-12-22 22:25:55 +0100

    finished design

--- src/SinglePriceGrid.tsx
+++ src/SinglePriceGrid.tsx
@@ -1,16 +1,17 @@
 function SinglePriceGrid() {
     return (<div className={'price-grid'}>
-        <div>
+        <div className={'col-span-2'}>
             <h2>Join our community</h2>
             <h3>30-day, hassle-free money back guarantee</h3>
-            <p>Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers
+            <p className={'access'}>Gain access to our full library of tutorials along with expert code reviews. Perfect
+                for any developers
                 who are serious about honing their skills</p>
         </div>
         <div>
             <h2>Monthly Subscription</h2>
-            <div><span className={'price'}>$29</span> per month</div>
+            <div className={'flex-group'}><span className={'price'}>$29</span> per month</div>
             <p>Full access for less than $1 a day</p>
-            <a>Sign Up</a>
+            <a href="#">Sign Up</a>
         </div>
         <div>
             <h2>Why Us</h2>
